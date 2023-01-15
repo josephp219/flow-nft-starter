b4923e89a0553ad05b8b4f86444f2336c61a0c5e
Joseph <joseph.jp2190@gmail.com>
2023-01-15 06:45:01 -0700

    Add minting functions

--- src/cadence/scripts/getTotalSupply_script.js
+++ src/cadence/scripts/getTotalSupply_script.js
@@ -1,7 +1,7 @@
 export const getTotalSupply =
 // REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
 `
-import BottomShot from 0x7b6adb682517f137;
+import BottomShot from 0xa61a2dd48f1e1a63;
 
 pub fun main(): UInt64 {
 
